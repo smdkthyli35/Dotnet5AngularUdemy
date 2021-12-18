e051ac4ad037cb2b2375bc4d2646804f234e4cef
zeljkosamed <smdkthyli@gmail.com>
2021-12-18 17:08:44 +0300

    Account controller oluşturuldu ve login işlemi yapıldı.

---
+++ WebAPI/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using Entities.Concrete.Identity;
+using Entities.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+            if (!result.Succeeded)
+            {
+                return Unauthorized();
+            }
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = "Token değeri alındı!",
+                DisplayName = user.DisplayName
+            };
+        }
+    }
+}
