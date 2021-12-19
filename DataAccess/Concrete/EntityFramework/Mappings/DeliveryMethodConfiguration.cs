a31d5027d75c2dc9ff6f7ba0e037376b38e5635d
zeljkosamed <smdkthyli@gmail.com>
2021-12-19 14:57:43 +0300

    Yeni classlardan bazılarına mapping işlemi uygulandı.

---
+++ DataAccess/Concrete/EntityFramework/Mappings/DeliveryMethodConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Mappings
+{
+    public class DeliveryMethodConfiguration : IEntityTypeConfiguration<DeliveryMethod>
+    {
+        public void Configure(EntityTypeBuilder<DeliveryMethod> builder)
+        {
+            builder.Property(i => i.Price).HasColumnType("decimal(18,2)");
+        }
+    }
+}
